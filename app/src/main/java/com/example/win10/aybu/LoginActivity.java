f9171d4576365184b095d50a252f0c180330343e
sg1907 <yildizseyitgazi@gmail.com>
2018-05-14 21:21:56 +0300

    Login and FAN

--- app/src/main/java/com/example/win10/aybu/LoginActivity.java
+++ app/src/main/java/com/example/win10/aybu/LoginActivity.java
@@ -13,60 +13,90 @@ import android.widget.MultiAutoCompleteTextView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.example.win10.aybu.Model.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class LoginActivity extends AppCompatActivity {
 
-    private Button btn_signIn;
-    private EditText editText_username;
-    private EditText editText_password;
+    FirebaseDatabase database;
+    DatabaseReference users;
 
-    private Spinner spinner_department;
-
-    List<String> categories = new ArrayList<String>();
+    private EditText username, password;
+    private Button btnSignIn, btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        btn_signIn = (Button) findViewById(R.id.button_signIn);
-        editText_username = (EditText) findViewById(R.id.editText_userName);
-        editText_password = (EditText) findViewById(R.id.editText_password);
+        //Firebase
+        database = FirebaseDatabase.getInstance();
+        users = database.getReference("Users");
 
-        spinner_department = (Spinner) findViewById(R.id.spinner_department);
-
-        categories.add("Department");
-        categories.add("Comp. Eng");
-        categories.add("Elec. Eng");
-        categories.add("Mach. Eng");
-        categories.add("Civil Eng.");
-        categories.add("Mater. Eng");
+        username = (EditText) findViewById(R.id.edtTxt_username);
+        password = (EditText) findViewById(R.id.edtTxt_password);
+        btnSignIn = (Button) findViewById(R.id.button_signIn);
+        btnSignUp = (Button) findViewById(R.id.button_signUp);
 
 
-        // Creating adapter for spinne
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
-
-        // Drop down layout style - list view with radio button
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        // attaching data adapter to spinner
-        spinner_department.setAdapter(dataAdapter);
-
-
-        final Intent in = new Intent(this,MainActivity.class);
-
-        btn_signIn.setOnClickListener(new View.OnClickListener() {
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                startActivity(in);
-
+                UserControl(username.getText().toString(),password.getText().toString());
             }
         });
 
+        final Intent in=  new Intent(getApplicationContext(),SignUpActivity.class);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(in);
+            }
+        });
+    }
+
+    private void UserControl(final String username, final String password) {
+
+        final Intent i=  new Intent(getApplicationContext(),MainActivity.class);
+
+        users.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.child(username).exists()){
+
+                    if(!username.isEmpty()){
+
+                        User login = dataSnapshot.child(username).getValue(User.class);
+
+                        if(login.getPassword().equals(password)){
+                            Toast.makeText(getApplicationContext(),"Success Login!",Toast.LENGTH_SHORT).show();
+                            startActivity(i);
+                        }
+                        else{
+                            Toast.makeText(getApplicationContext(),"Password is Wrong!",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    else{
+                        Toast.makeText(getApplicationContext(),"Username is Not Registered!",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
 }
